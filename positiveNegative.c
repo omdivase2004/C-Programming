bae754b96e3cf07ab43f9902bc8b1838e985928e
unknown <omdivase2004@gmail.com>
2025-06-11 14:24:47 +0530

    first commit

---
+++ positiveNegative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter a number");
+scanf("%d",&n);
+if(n>0)
+printf("Positive No");
+else
+printf("Negative No");
+return 0;
+
+
+}
---
+++ ternary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number");
+    scanf("%d",&n);
+    
+    (n>0)?printf("positive no"):printf("negative no");
+    return 0;
+}
